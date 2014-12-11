919844d73d93096afa2d69eb49a340180306091e
JustinKon <jkonersmann@smu.edu>
2014-12-11 00:57:22 -0600

    Created another asynchronous task to grab a pic with a specific url to test displaying images without pre loading them.

--- RandomCats/src/com/cse3345/randomcats/MainActivity.java
+++ RandomCats/src/com/cse3345/randomcats/MainActivity.java
@@ -4,6 +4,10 @@
 package com.cse3345.randomcats;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.ParseException;
@@ -14,16 +18,23 @@ import org.apache.http.util.EntityUtils;
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity {
 	//Asynchronous Task
 	private Context context;
 	private ProgressDialog progressDialog;
+	
+	private ImageView mainImage;
+	private Drawable pic;
+	
 	private String catApiUrl = "http://thecatapi.com/api/images/get?format=xml";
+	private String sampleCatUrl = "http://24.media.tumblr.com/tumblr_m1jwx7Idy41qfhy97o1_500.jpg";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -32,8 +43,13 @@ public class MainActivity extends Activity {
 		
 		context = MainActivity.this;
 		
+		mainImage = (ImageView) findViewById(R.id.mainImage);
+		
 		getCatAsync task = new getCatAsync();
 		task.execute((Object[]) null);
+		
+		//getLinksAsync task = new getLinksAsync();
+		//task.execute((Object[]) null);
 	}
 
 	@Override
@@ -61,6 +77,51 @@ public class MainActivity extends Activity {
 		@Override
 		protected void onPreExecute() {
 			progressDialog = new ProgressDialog(context);
+			progressDialog.setTitle("Loading a cat pic...");
+			progressDialog.setMessage(":3");
+			progressDialog.setCancelable(false);
+			progressDialog.setIndeterminate(true);
+			progressDialog.show();
+		}
+
+		@Override
+		protected Object doInBackground(Object... params) {
+			try {
+				InputStream is = (InputStream) new URL(sampleCatUrl).getContent();
+				pic = Drawable.createFromStream(is, "");
+			} catch (MalformedURLException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			if(pic.toString() != null){
+				success = true;
+			}
+			return null;
+		}
+
+		@Override
+		protected void onPostExecute(Object result) {
+			if (progressDialog != null) {
+				progressDialog.dismiss();
+			}
+			if (success) {
+				System.out.println("Pic: " + pic);
+				System.out.println("Load pic");
+				//This is a success full URL hit
+				mainImage.setImageDrawable(pic);
+			} else {
+				System.out.println("Failure.");
+			}
+		}
+	}
+	
+	private class getLinksAsync extends AsyncTask<Object, Object, Object> {
+		private boolean success = false;
+
+		@Override
+		protected void onPreExecute() {
+			progressDialog = new ProgressDialog(context);
 			progressDialog.setTitle("Loading cats...");
 			progressDialog.setMessage(":3");
 			progressDialog.setCancelable(false);
@@ -79,7 +140,6 @@ public class MainActivity extends Activity {
 				HttpEntity httpEntity = httpResponse.getEntity();
 				response = EntityUtils.toString(httpEntity);
 			} catch (ParseException | IOException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 			System.out.println(response);
