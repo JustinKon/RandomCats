529a895ce316d9aaddc1cb791e8ac4960caf8116
JustinKon <jkonersmann@smu.edu>
2014-12-11 00:04:58 -0600

    Added an Asyncronous call to the catapi.com. Successfully retrieve xml and ouput to console.

--- RandomCats/src/com/cse3345/randomcats/MainActivity.java
+++ RandomCats/src/com/cse3345/randomcats/MainActivity.java
@@ -1,16 +1,39 @@
+/**
+ * Author: Justin Konersmann
+ * */
 package com.cse3345.randomcats;
 
+import java.io.IOException;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.ParseException;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends Activity {
+	//Asynchronous Task
+	private Context context;
+	private ProgressDialog progressDialog;
+	private String catApiUrl = "http://thecatapi.com/api/images/get?format=xml";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		context = MainActivity.this;
+		
+		getCatAsync task = new getCatAsync();
+		task.execute((Object[]) null);
 	}
 
 	@Override
@@ -30,5 +53,47 @@ public class MainActivity extends Activity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+	
+	private class getCatAsync extends AsyncTask<Object, Object, Object> {
+		private boolean success = false;
+
+		@Override
+		protected void onPreExecute() {
+			progressDialog = new ProgressDialog(context);
+			progressDialog.setTitle("Loading cats...");
+			progressDialog.setMessage(":3");
+			progressDialog.setCancelable(false);
+			progressDialog.setIndeterminate(true);
+			progressDialog.show();
+		}
+
+		@Override
+		protected Object doInBackground(Object... params) {
+			String response = null;
+			
+			try {
+				DefaultHttpClient httpClient = new DefaultHttpClient();
+				HttpPost httpPost = new HttpPost(catApiUrl);
+				HttpResponse httpResponse = httpClient.execute(httpPost);
+				HttpEntity httpEntity = httpResponse.getEntity();
+				response = EntityUtils.toString(httpEntity);
+			} catch (ParseException | IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			System.out.println(response);
+			return null;
+		}
+
+		@Override
+		protected void onPostExecute(Object result) {
+			if (progressDialog != null) {
+				progressDialog.dismiss();
+			}
+			if (success) {
+				//This is a success full URL hit
+			}
+		}
 	}
 }
